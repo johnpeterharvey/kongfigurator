ce3f1f4d2512cf83df98a52c9151f7f17f2558b3
John Peter Harvey <john.harvey@travelex.com>
2015-10-12 12:55:35 +0100

    Base ruby script

---
+++ main.rb
@@ -0,0 +1,64 @@
+require 'net/http'
+require 'yaml'
+
+def get_kong_url
+  if !ENV.has_key? 'KONG_URL'
+    puts 'Set KONG_URL environment variable!'
+    exit 1
+  end
+  URI(ENV['KONG_URL'])
+end
+
+def get_composure
+  composure_file = File.new('docker-compose.yml')
+  if composure_file.nil?
+    puts 'Add docker-compose.yml'
+    exit 2
+  end
+  YAML.load(composure_file)
+end
+
+def check_kong_reachable(kong_url)
+  while true do
+    puts "Trying to reach Kong..."
+    if Net::HTTP.get(kong_url).is_a?(Net::HTTPSuccess)
+      break
+    end
+    sleep(5)
+  end
+end
+
+def register_apis(kong_url, composure)
+  composure.each do |container|
+    if !container[1].has_key? 'labels'
+      if !container[1]['labels']['kong_register'] == 'true'
+        next
+      end
+    end
+
+    puts "Container #{container[0]} is Kong enabled"
+    konfig = container[1]['labels'].reject {|key, value| !key.starts_with 'kong_'}
+
+    form_data = {
+      'upstream_url' => konfig['kong_upstream_url'],
+      'request_path' => konfig.has_key?('kong_version') ? "/#{konfig['kong_version']}/#{container[0]}" : "/#{container[0]}",
+      'name' => container[0]
+    }
+    if konfig.has_key? 'kong_strip_request_path'
+      form_data['strip_request_path'] = konfig['kong_strip_request_path']
+    end
+
+    puts "\tSending registration data #{form_data}"
+    result = Net::HTTP.post_form(kong_url, form_data)
+    puts "\tRegistering #{container[0]} got return code #{result}"
+  end
+end
+
+kong_url = get_kong_url
+puts "Read Kong URL #{kong_url}"
+
+composure = get_composure
+
+check_kong_reachable(kong_url)
+
+register_apis(kong_url, composure)
